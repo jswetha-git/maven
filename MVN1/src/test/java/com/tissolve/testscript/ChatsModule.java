93ec08f94252f43e5e361843b2ebbd68fbbf38b2
J.swetha <J.swetha@LAPTOP-4615C71G>
2025-03-09 13:59:45 +0530

    jenkinsfirsttest

--- MVN1/src/test/java/com/tissolve/testscript/ChatsModule.java
+++ MVN1/src/test/java/com/tissolve/testscript/ChatsModule.java
@@ -1,5 +1,11 @@
 package com.tissolve.testscript;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.decorators.WebDriverDecorator;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -7,6 +13,11 @@ public class ChatsModule {
 	@Test
 	public void test()
 	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("www.facebook.com");
+		System.out.println(driver.getTitle());
 		Reporter.log("HELLO",true);
 	}
 
